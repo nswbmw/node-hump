064df96c0e6ef0edbe7ac2687f98063483367957
nswbmw <gxqzk@126.com>
2014-03-17 12:59:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+## node-hump ##
+
+convert a string(like: hello-world) to hump-style string(to: helloWorld)
+
+### Install ###
+
+    npm install hump
+
+### Usage ###
+
+    var hump = require('hump');
+    console.log()
---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = function(str, separator) {
+  var strArray = str.split(separator);
+  var humpStr = strArray[0];
+  for (var i = 1, l = strArray.length; i < l; i+=1) {
+  	humpStr += strArray[i].slice(0, 1).toUpperCase() + strArray[i].slice(1);
+  }
+  return humpStr;
+}
